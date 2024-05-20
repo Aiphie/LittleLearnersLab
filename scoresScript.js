69117cae4ab5f77c2894801ec4b13ad164af43e7
Aiphie <sbmagkasi@gmail.com>
2024-05-20 20:12:32 +0800

    individual project files

---
+++ scoresScript.js
@@ -0,0 +1,9 @@
+function showScore(){
+    const ScoresAll=[];
+    //var latestScore= document.cookie;
+    var alphaScoresArr = JSON.parse(localStorage.getItem("alphaScore"));
+
+    console.log(JSON.parse(localStorage.getItem("alphaScore")));
+    
+    //document.getElementById('abcScore').innerHTML='Alphabet Quiz: '+abcScore;
+}
