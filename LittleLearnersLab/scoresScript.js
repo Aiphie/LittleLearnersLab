e4dd486bd6b4af5709503786e0dffbc0d696b110
Aiphie <sbmagkasi@gmail.com>
2024-05-20 20:11:38 +0800

    removed folder

--- LittleLearnersLab/scoresScript.js
+++
@@ -1,9 +0,0 @@
-function showScore(){
-    const ScoresAll=[];
-    //var latestScore= document.cookie;
-    var alphaScoresArr = JSON.parse(localStorage.getItem("alphaScore"));
-
-    console.log(JSON.parse(localStorage.getItem("alphaScore")));
-    
-    //document.getElementById('abcScore').innerHTML='Alphabet Quiz: '+abcScore;
-}
